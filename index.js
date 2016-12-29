38d9a1d313295a721a1e90ef728ae4f6c0616201
vishnuprasad <vishnuprasad.s@atomicits.com>
2016-12-29 19:00:35 +0530

    exporting native modules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var DigitsManager = require("react-native").NativeModules.DigitsManager;
+var NativeModules = require("react-native").NativeModules;
 var DigitsLoginButton = require('./DigitsLoginButton');
 var DigitsLogoutButton = require('./DigitsLogoutButton');
 
 module.exports.DigitsLoginButton = DigitsLoginButton;
 module.exports.DigitsLogoutButton = DigitsLogoutButton;
-module.exports.DigitsManager = DigitsManager;
+module.exports.NativeModules = NativeModules;
